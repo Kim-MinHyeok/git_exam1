6dc5c7858e8d19a5c6d414f9731d25b7fc6c9b55
Kim-MinHyeok <48408314+Kim-MinHyeok@users.noreply.github.com>
2021-09-28 09:34:40 +0900

    201534005-4

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 package git_exam1;
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("글로컬IT학과");
-		System.out.println("201534005");
-		System.out.println("김민혁"); 
+		System.out.println("소속: 글로컬IT학과");
+		System.out.println("학번: 201534005");
+		System.out.println("이름: 김민혁"); 
 	}
 }
